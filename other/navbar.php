55f99fc90205acb0208df1dbd6344ef11a159059
johan_chou <john.hello2u@gmail.com>
2018-04-22 17:42:10 +0200

    made a navbar php file

---
+++ other/navbar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="stylesheet/navbarStyle.css">
+  </head>
+  <body>
+
+    <div class="nav-bar">
+      <nav>
+        <div class="handle">
+          <div class="icon"><div class="hamburger-menu"></div></div>
+        </div>
+        <ul id="nav-ul">
+          <li> <a href="#">home</a> </li>
+          <li> <a href="#">friends</a></li>
+          <li> <a href="#">settings</a></li>
+        </ul>
+      </nav>
+    </div>
+
+  </body>
+</html>
