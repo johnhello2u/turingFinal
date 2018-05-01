33b1dc0ae891b89fffc0b835ff75c07f68dca27a
johan_chou <john.hello2u@gmail.com>
2018-05-01 18:00:58 +0200

    nothing special

--- database.php
+++ database.php
@@ -1,7 +1,10 @@
 <?php
 
 
-$connect = mysqli_connect("localhost", "root", "Luther&vanity?911","twitter");
+
+
+
+$connect = mysqli_connect("localhost", "root", "","twitter");
 if ($connect) {
 	echo "Conncetion Successful !!! ";
 }
