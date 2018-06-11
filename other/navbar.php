e00d7beef163de12bfda62693513be878f832b25
johan_chou <john.hello2u@gmail.com>
2018-06-11 08:12:59 +0200

    a new clean slate

--- other/navbar.php
+++ other/navbar.php
@@ -10,29 +10,25 @@
   <body>
 
 
-      <nav class="navbar navbar-expand-lg navbar-light">
+<nav class="navbar navbar-expand-lg navbar-light">
+
+  <a class="navbar-brand" href="index.php">
+      <img src="images/logo.jpg" width="50" height="50" style="border-radius: 50%;margin:0px 15px;" alt="">
+  </a>
+
+  <!-- <form class="form-inline my-2 my-lg-0">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
+  </form> -->
+
+  <ul>
+    <li class = "navlist"> <a href="index.php">Home</a> </li>
+    <li class = "navlist"> <a href="#">Sign up</a></li>
+    <li class = "navlist"> <a href="#">About us</a></li>
+  </ul>
+</nav>
 
 
-        <a class="navbar-brand" href="#">
-        <a class="navbar-brand" href="index.php">
-          <img src="images/logo.jpg" width="70" height="70" style="border-radius: 50%;margin:0px 15px;" alt="">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-
-        <ul id="nav-ul">
-
-          <li class = "navlist"> <a href="index.php">home</a> </li>
-          <li class = "navlist"> <a href="#">Sign up</a></li>
-          <li class = "navlist"> <a href="#">B</a></li>
-
-      </nav>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
